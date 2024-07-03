c64a4b5c580b8dc78516f003a006239453b280d1
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-03 18:07:45 -0400

    randomly pick the dice image

---
+++ index.js
@@ -0,0 +1,5 @@
+let randomNumber1=Math.floor((Math.random()*6))+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+
+let randomNumber2=Math.floor((Math.random()*6))+1;
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
