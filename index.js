887a8ae34d3e49ca57b4d5f1c20cf77f85bc8aaf
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-03 20:51:01 -0400

    show result accordingly

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+//Randomly generate 2 numbers (1-6)
 let randomNumber1=Math.floor((Math.random()*6))+1;
 document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
 
 let randomNumber2=Math.floor((Math.random()*6))+1;
 document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+//check winner and display
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins <img style='width:100px' src='./images/red-flag.png' alt='winner-flag'>";
+}else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins <img style='width:100px' src='./images/red-flag.png' alt='winner-flag'>";
+}else{
+    document.querySelector("h1").innerHTML="Draw!!";
+}
